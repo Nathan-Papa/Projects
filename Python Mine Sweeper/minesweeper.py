b10873183e2711aad1c02057c65fb57c04c21f37
Nathan Papa <nathanpapa35@gmail.com>
2023-01-03 20:45:55 -0600

    edited minesweeper and snake game

---
+++ Python Mine Sweeper/minesweeper.py
@@ -0,0 +1,10 @@
+import pygame
+import random
+import time
+
+pygame.init()
+side_length = 524
+
+gameboard = pygame.display.set_mode((side_length, side_length))
+pygame.display.set_caption("Mine Sweeper")
+
--- Python Snake Game/snake.py
+++ Python Snake Game/snake.py
@@ -4,15 +4,14 @@ import random
  
 pygame.init()
 gameScreen = pygame.display.set_mode((600, 600))
-pygame.display.set_caption("Python Snake Game")
+pygame.display.set_caption("Snake Game")
 
-carryOn = True
 x = 240
 y = 285
  
 clock = pygame.time.Clock()
 
-while carryOn:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
               carryOn = False
